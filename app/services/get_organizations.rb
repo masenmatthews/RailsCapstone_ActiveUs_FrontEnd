74bb42c984ad67326f0e14f2cd0fb310817061b4
Masen Matthews <masenmatthews@gmail.com>
2018-05-16 21:54:01 -0700

    troubleshoot with parse loop

--- app/services/get_organizations.rb
+++ app/services/get_organizations.rb
@@ -1,15 +1,18 @@
 class Organization
   def self.get_homeless_organizations
-    response = RestClient.get('http://localhost:3001/organizations?category=Homelessness')
-    binding.pry
-    JSON.parse(response)["name"]["category"]["description"]["address"]["phone"]["website"]
+    response = RestClient.get('http://localhost:3001/organizations?category=Homelessness'),
+      JSON.parse(response)
+      response.each do |resource|
+        binding.pry
+        puts resource.name
+      end
   end
-  def self.get_music_and_art_organizations
-    response = RestClient.get('http://localhost:3001/organizations?category=Music+and+Art')
-    JSON.parse(response)["name"]["category"]["description"]["address"]["phone"]["website"]
-  end
-  def self.get_animal_welfare_organizations
-    response = RestClient.get('http://localhost:3001/organizations?category=Animal+welfare')
-    JSON.parse(response)["name"]["category"]["description"]["address"]["phone"]["website"]
-  end
+  # def self.get_music_and_art_organizations
+  #   response = RestClient.get('http://localhost:3001/organizations?category=Music+and+Art')
+  #   JSON.parse(response)["name"]["category"]["description"]["address"]["phone"]["website"]
+  # end
+  # def self.get_animal_welfare_organizations
+  #   response = RestClient.get('http://localhost:3001/organizations?category=Animal+welfare')
+  #   JSON.parse(response)["name"]["category"]["description"]["address"]["phone"]["website"]
+  # end
 end
