0c42a8001f1f66b767998f09327032244f6b8189
Masen Matthews <masenmatthews@gmail.com>
2018-05-18 17:45:44 -0700

    troubleshoot JSON parse

--- app/services/get_organizations.rb
+++ app/services/get_organizations.rb
@@ -1,10 +1,11 @@
 class Organization
   def self.get_homeless_organizations
-    response = RestClient.get('http://localhost:3001/organizations?category=Homelessness'),
+    binding.pry
+    response = RestClient.get('http://localhost:3001/organizations?category=Homelessness')
       JSON.parse(response)
-      response.each do |resource|
-        binding.pry
-        puts resource.name
+      response.each do |response|
+        puts response
+        puts "#{response['name']['description']['address']['phone']['website']}"
       end
   end
   # def self.get_music_and_art_organizations
