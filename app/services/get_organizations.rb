9325854afbf88269a6c22d96d7084911a80c4ea0
Masen Matthews <masenmatthews@gmail.com>
2018-05-18 18:29:07 -0700

    change view for homeless organization display

--- app/services/get_organizations.rb
+++ app/services/get_organizations.rb
@@ -1,19 +1,20 @@
 class Organization
   def self.get_homeless_organizations
-    binding.pry
     response = RestClient.get('http://localhost:3001/organizations?category=Homelessness')
       JSON.parse(response)
+      if response.code == '200' then
       response.each do |response|
         puts response
         puts "#{response['name']['description']['address']['phone']['website']}"
       end
+    end
   end
-  # def self.get_music_and_art_organizations
-  #   response = RestClient.get('http://localhost:3001/organizations?category=Music+and+Art')
-  #   JSON.parse(response)["name"]["category"]["description"]["address"]["phone"]["website"]
-  # end
-  # def self.get_animal_welfare_organizations
-  #   response = RestClient.get('http://localhost:3001/organizations?category=Animal+welfare')
-  #   JSON.parse(response)["name"]["category"]["description"]["address"]["phone"]["website"]
-  # end
+  def self.get_music_and_art_organizations
+    response = RestClient.get('http://localhost:3001/organizations?category=Music+and+Art')
+    JSON.parse(response)
+  end
+  def self.get_animal_welfare_organizations
+    response = RestClient.get('http://localhost:3001/organizations?category=Animal+welfare')
+    JSON.parse(response)
+  end
 end
