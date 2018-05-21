c513ffbecc17b8eebb2e9b516eaef2cfca6c31de
Masen Matthews <masenmatthews@gmail.com>
2018-05-21 10:02:06 -0700

    finish back end API call fixes - moving on to correcting views

--- app/services/get_organizations.rb
+++ app/services/get_organizations.rb
@@ -6,11 +6,15 @@ class Organization
     end
   end
   def self.get_music_and_art_organizations
-    response = RestClient.get('http://localhost:3001/organizations?category=Music+and+Art')
-    JSON.parse(response)
+    response = JSON.parse(RestClient.get('http://localhost:3001/organizations?category=Music+and+Art'))
+    response.each do |organization|
+      p organization.values_at('name', 'description', 'address', 'phone', 'website')
+    end
   end
   def self.get_animal_welfare_organizations
-    response = RestClient.get('http://localhost:3001/organizations?category=Animal+welfare')
-    JSON.parse(response)
+    response = JSON.parse(RestClient.get('http://localhost:3001/organizations?category=Animal+welfare'))
+    response.each do |organization|
+      p organization.values_at('name', 'description', 'address', 'phone', 'website')
+    end
   end
 end
